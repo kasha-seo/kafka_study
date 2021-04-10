84c4c2e6454ffbddeaba0f705469223b77e0964d
KaSha(서진혁) <kasha.seo@kakaocorp.com>
2021-04-10 17:35:44 +0900

    webMVC enable

--- src/main/kotlin/com/example/kafka_study/KafkaStudyApplication.kt
+++ src/main/kotlin/com/example/kafka_study/KafkaStudyApplication.kt
@@ -2,8 +2,10 @@ package com.example.kafka_study
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
+@EnableWebMvc
 class KafkaStudyApplication
 
 fun main(args: Array<String>) {
