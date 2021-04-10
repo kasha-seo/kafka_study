b96afccda3208d505bd71b91027ca7e2b2dbce73
KaSha(서진혁) <kasha.seo@kakaocorp.com>
2021-04-10 17:34:17 +0900

    spring-web 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,14 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //    implementation("org.apache.kafka:kafka-clients")
     implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-web-test")
 }
 
 tasks.withType<KotlinCompile> {
